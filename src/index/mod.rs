e1b2aa35ad7ffa2b7d9812f4f88c043926f7b266
Luiz Irber <luiz.irber@gmail.com>
2020-01-01 00:40:50 +0000

    spun off from sourmash

---
+++ src/index/mod.rs
@@ -0,0 +1,8 @@
+use sourmash::index::linear::LinearIndex;
+use sourmash::signature::Signature;
+
+use crate::sketch::ukhs::FlatUKHS;
+
+//pub mod ukhs;
+
+//pub type UKHSTree = SBT<Node<FlatUKHS>, Signature>;
